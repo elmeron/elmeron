40c62803ebc861901d8f4f1a624da7404591cefc
Natanael Log <natte.log@gmail.com>
2017-05-02 15:06:45 +0200

    add 'Empty' label on GemCard when empty

--- client/src/components/cards/GemCard.jsx
+++ client/src/components/cards/GemCard.jsx
@@ -5,6 +5,10 @@ import Card from './Card.jsx';
 import GemIcon from '../GemIcon.jsx';
 
 function GemCard(props) {
+  if (props.gems.isEmpty()) {
+    return <Card><p>Empty</p></Card>
+  }
+
   return (
     <Card>
       {props.gems.map(({ amount, resource }, index) =>
